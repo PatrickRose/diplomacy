336fb354e71f52a79e02b57340e9dea822e330a9
Patrick Rose <pjr0911025@googlemail.com>
2012-08-05 00:59:39 +0100

    Cleaned up feed/index.php

--- feed/index.php
+++ feed/index.php
@@ -1,31 +1,34 @@
 <?php
     header("Content-Type: application/rss+xml; charset=ISO-8859-1"); 
     include ('feedconnect.php');
-    $rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
-    $rssfeed .= '<rss version="2.0">';
-    $rssfeed .= '<channel>';
-    $rssfeed .= '<title>Diplomacy State of Play</title>';
-    $rssfeed .= '<link>http://diplomacy.patrickrosemusic.co.uk</link>';
-    $rssfeed .= '<description>This is the RSS feed for the game of anon diplomacy that I\'m </description>';
-    $rssfeed .= '<language>en-uk</language>';
-    $rssfeed .= '<copyright>Copyright (C) 2012 Patrick Rose</copyright>';
+    $rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>
+        <rss version="2.0">
+            <channel>
+                <title>Diplomacy State of Play</title>
+                <link>http://diplomacy.patrickrosemusic.co.uk</link>
+                <description>This is the RSS feed for the game of anon diplomacy that I\'m </description>
+                <language>en-uk</language>
+                <copyright>Copyright (C) 2012 Patrick Rose</copyright>';
     $connection = create_connection();
- 
     $query = "SELECT * FROM summary ORDER BY turnNum DESC";
     $result = mysql_query($query, $connection) or die ("Could not execute query");
     while($row = mysql_fetch_array($result)) {
         extract($row);
- 
-        $rssfeed .= '<item>';
-        $rssfeed .= '<title>Diplomacy Turn ' . $turnNum . '</title>';
-        $rssfeed .= '<description>' . $description . '</description>';
-        $rssfeed .= '<link>http://diplomacy.patrickrosemusic.co.uk/#turn' . $turnNum . '</link>';
-        $rssfeed .= '<pubDate>' . date("D, d M Y H:i:s O", strtotime($date)) . '</pubDate>';
-        $rssfeed .= '</item>';
+        $link = 'http://diplomacy.patrickrosemusic.co.uk/#turn' . $turnNum;
+        $pubDate = date("D, d M Y H:i:s O", strtotime($date));
+        $rssfeed .=
+            '<item>
+                <title>Diplomacy Turn $turnNum</title>
+                <description>$description</description>
+                <link>$link</link>
+                <pubDate>$pubDate</pubDate>
+            </item>';
     }
  
-    $rssfeed .= '</channel>';
-    $rssfeed .= '</rss>';
+    $rssfeed .=
+        '
+            </channel>
+        </rss>';
  
     echo $rssfeed;
 
